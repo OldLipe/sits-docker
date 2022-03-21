4ee370de2ccd9ec13a76411bca906fdaee092a19
Felipe <lipecaso@gmail.com>
2022-03-21 11:56:06 -0300

    update code

---
+++ docker/r/.buid.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t sits-dev/sits:0.16.3 --file Dockerfile  .
--- docker/sits/install.R
+++ docker/sits/install.R
@@ -9,7 +9,7 @@
 # Satellite Image Time Series
 #
 sits_tag_version <- Sys.getenv("SITS_TAG_VERSION")
-remotes::install_github(paste0("e-sensing/sits@", sits_tag_version), dependencies = FALSE)
+remotes::install_github(paste0("e-sensing/sits@dev"), dependencies = FALSE)
 
 #
 # Satellite Image Time Series Example Datasets
